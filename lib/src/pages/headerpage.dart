49aba11a158684c97abf5a6f3d17855e1982eee5
escobarfrancisco <escobarfrancisco9905@gmail.com>
2022-10-26 09:44:03 -0500

    DiseñoVistaCanvas

---
+++ lib/src/pages/headerpage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:from_end/src/widgets/view.dart';
+
+class Headerpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ViewWelcome(), //canvas
+    );
+  }
+}
