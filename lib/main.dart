4326345c84236973b61fcb74d7e7f83a52ee54a0
escobarfrancisco <escobarfrancisco9905@gmail.com>
2022-11-07 02:11:56 -0600

    Actualizacion DetallesMenu

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:from_end/src/pages/pageHome.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
--- lib/src/widgets/viewOptions.dart
+++ lib/src/widgets/viewOptions.dart
@@ -42,20 +42,20 @@ class _main_viewState extends State<viewoptions> {
     // ignore: prefer_const_literals_to_create_immutables, dead_code
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xf305718D),
+        backgroundColor: const Color(0xff29859a),
       ),
       drawer: Drawer(
-        backgroundColor: const Color(0xf305718D),
+        backgroundColor: const Color(0xff29859a),
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
             DrawerHeader(
-              decoration: const BoxDecoration(color: Color(0xf305718D)),
+              decoration: const BoxDecoration(color: Color(0xff127990)),
               child: Column(
                 children: [
-                  /*Expanded(
-                    child: Image.asset('assets/images/perfil.png'),
-                  ),*/
+                  Expanded(
+                    child: Image.asset('assets/images/perfil2.png'),
+                  ),
                   const Text("gaby gomez"),
                   // ignore: prefer_const_constructors
                   SizedBox(
@@ -65,31 +65,35 @@ class _main_viewState extends State<viewoptions> {
               ),
             ),
             ListTile(
-              tileColor: const Color(0xf305718D),
+              tileColor: const Color(0xff29859a),
               selectedTileColor: const Color(0xf305718D),
               leading: Icon(Icons.home),
-              title: Text("Inicion"),
+              title: Text("Inicio"),
+              textColor: Color(0xffe4f2fb),
               onTap: () {},
             ),
             ListTile(
-              tileColor: const Color(0xf305718D),
+              tileColor: const Color(0xff29859a),
               selectedTileColor: const Color(0xf305718D),
               leading: Icon(Icons.soap_outlined),
               title: Text("Solicitud"),
+              textColor: Color(0xffe4f2fb),
               onTap: () {},
             ),
             ListTile(
-              tileColor: const Color(0xf305718D),
+              tileColor: const Color(0xff29859a),
               selectedTileColor: const Color(0xf305718D),
               leading: Icon(Icons.military_tech_sharp),
               title: Text("Miembros"),
+              textColor: Color(0xffe4f2fb),
               onTap: () {},
             ),
             ListTile(
-              tileColor: const Color(0xf305718D),
+              tileColor: const Color(0xff29859a),
               selectedTileColor: const Color(0xf305718D),
               leading: Icon(Icons.manage_accounts),
               title: Text("Configuraciones"),
+              textColor: Color(0xffe4f2fb),
               onTap: () {},
             ),
           ],
@@ -102,19 +106,19 @@ class _main_viewState extends State<viewoptions> {
       bottomNavigationBar: BottomNavigationBar(
           currentIndex: _index,
           type: BottomNavigationBarType.fixed,
-          backgroundColor: const Color(0xf305718D),
+          backgroundColor: const Color(0xff07708c),
           onTap: (index) => setState(() {
                 _index = index;
               }),
           items: const [
             BottomNavigationBarItem(
-                icon: Icon(Icons.messenger_outline_sharp), label: "mesaguer"),
+                icon: Icon(Icons.messenger_outline_sharp), label: "Mensajes"),
             BottomNavigationBarItem(
-                icon: Icon(Icons.add_box_outlined), label: "add"),
+                icon: Icon(Icons.add_box_outlined), label: "Agregar"),
             BottomNavigationBarItem(
                 icon: Icon(Icons.notifications_none_outlined),
-                label: "notificasion"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "datos"),
+                label: "Notificaciones"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Perfil"),
           ]),
     );
   }
