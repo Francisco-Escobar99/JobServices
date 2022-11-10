32e28b97c31e7917cbd694a3eaf8dcae3820975a
escobarfrancisco <escobarfrancisco9905@gmail.com>
2022-11-10 01:11:57 -0600

    CreacionVistaPassword

---
+++ lib/src/pages/pagePassword.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:from_end/src/widgets/viewPassword.dart';
+
+class passwordPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Password_view(), //canvas
+    );
+  }
+}
--- lib/src/widgets/viewPassword.dart
+++ lib/src/widgets/viewPassword.dart
@@ -1,11 +1,9 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:from_end/src/pages/pageOptions.dart';
-import 'package:from_end/src/pages/pageHome.dart';
-import 'package:from_end/src/pages/pagePassword.dart';
+import 'package:from_end/src/pages/pageLogin.dart';
 
-class view_login extends StatelessWidget {
-  const view_login({Key? key}) : super(key: key);
+class Password_view extends StatelessWidget {
+  const Password_view({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +11,7 @@ class view_login extends StatelessWidget {
       height: double.infinity,
       width: double.infinity,
       child: CustomPaint(
-        painter: _BackgroundLogin(),
+        painter: _BackgroundPassword(),
         child: ListView(
           children: [
             Row(
@@ -27,7 +25,7 @@ class view_login extends StatelessWidget {
                   alignment: Alignment.topLeft,
                   onPressed: () {
                     Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Headerpage()));
+                        MaterialPageRoute(builder: (context) => loginPage()));
                   },
                 ),
                 Image.asset(
@@ -37,83 +35,46 @@ class view_login extends StatelessWidget {
                 )
               ],
             ),
-            /* Container(
-              alignment: Alignment(0, 2),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xff04738b),
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(10)),
-                child: const Icon(Icons.arrow_back_ios, size: 80),
-                onPressed: () {},
-              ),
-            ),*/
             Container(
               padding: const EdgeInsets.only(
                   top: 70, bottom: 40, right: 30, left: 30),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    hintText: 'Correo Electronico',
-                    prefixIcon: Icon(Icons.email)),
-              ),
+              // ignore: prefer_const_constructors
+              child: Text('¿Olvidaste tu contraseña?',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                      color: Color(0xff06708d))),
             ),
             Container(
-              padding: const EdgeInsets.only(right: 30, left: 30),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    hintText: 'Contraseña', prefixIcon: Icon(Icons.lock_reset)),
-              ),
-            ),
-            TextButton(
+              padding: const EdgeInsets.only(
+                  top: 0, bottom: 30, right: 30, left: 30),
+              // ignore: prefer_const_constructors
+              //Padding(padding: EdgeInsets.symmetric(horizontal: 50)),
               child: Text(
-                "¿Olvidaste tu Contraseña?",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    color: Color(0xff4495a6)),
+                'Ingresa su correo electrico, te enviaremos las instrucciones para iniciar sesion',
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 17, color: Color(0xff569fb0)),
               ),
-              style: ButtonStyle(alignment: Alignment(0.7, 0.7)),
-              onPressed: () {
-                Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => passwordPage()));
-              },
             ),
             Container(
-              padding: const EdgeInsets.only(
-                right: 38,
-                left: 38,
-                top: 50,
-              ),
-              child: ElevatedButton(
-                child: Text('Iniciar Sesion',
-                    style:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => optionPage()));
-                },
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.white),
-                    padding:
-                        MaterialStateProperty.all(const EdgeInsets.all(15)),
-                    foregroundColor:
-                        MaterialStateProperty.all(Color(0xff208097)),
-                    side: MaterialStateProperty.all(const BorderSide(
-                      color: Color(0xff056980),
-                      width: 2,
+              padding: const EdgeInsets.only(top: 10, right: 30, left: 30),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    hintText: 'Correo electronico',
+                    prefixIcon: Icon(
+                      Icons.email,
                     )),
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(21)))),
               ),
             ),
             Container(
               padding: const EdgeInsets.only(
                 right: 38,
                 left: 38,
-                top: 40,
+                top: 70,
               ),
               child: ElevatedButton(
-                child: Text('Iniciar Sesion con Gmail',
+                child: Text('Enviar',
                     style:
                         TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                 onPressed: () {},
@@ -138,7 +99,7 @@ class view_login extends StatelessWidget {
   }
 }
 
-class _BackgroundLogin extends CustomPainter {
+class _BackgroundPassword extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final paint = new Paint();
